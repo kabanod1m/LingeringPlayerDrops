960b7a0fb26941d500e1989b5811aeea2f585b82
BuildTools <unconfigured@null.spigotmc.org>
2024-02-14 11:51:12 +0300

    Minor optimization of PlayerEntityMixin

--- src/main/java/org/winglessbirds/lingeringplayerdrops/mixin/PlayerEntityMixin.java
+++ src/main/java/org/winglessbirds/lingeringplayerdrops/mixin/PlayerEntityMixin.java
@@ -1,10 +1,7 @@
 package org.winglessbirds.lingeringplayerdrops.mixin;
 
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -13,11 +10,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.winglessbirds.lingeringplayerdrops.util.PlayerDropItemEvents;
 
 @Mixin(PlayerEntity.class)
-public abstract class PlayerEntityMixin extends LivingEntity {
-    protected PlayerEntityMixin(EntityType<? extends LivingEntity> entityType, World world) {
-        super(entityType, world);
-    }
-
+public abstract class PlayerEntityMixin {
     @Inject(method = "dropItem(Lnet/minecraft/item/ItemStack;ZZ)Lnet/minecraft/entity/ItemEntity;", at = @At("RETURN"))
     private void inject_dropItem(final ItemStack stack, final boolean throwRandomly, final boolean retainOwnership, @Nullable final CallbackInfoReturnable ret) {
         PlayerDropItemEvents.AFTER.invoker().afterDropItem(stack, throwRandomly, retainOwnership, ret);
